98859e2fd0e306e0b2e136e807bfa83d7ec52343
unknown <jfeeney010@hotmail.com>
2024-04-21 20:01:54 +0000

    Adding code rom python I

--- applied_db.py
+++ applied_db.py
@@ -1,8 +1,29 @@
-# Python app for Appiled Databases module - ATU Galway
+# Python app for Applied Databases module - ATU Galway
 # By Joanne Feeney
 
-pip install pymysql
 
 # Imports
+import pymysql
+
 import ("C:\Users\appDB\Documents\applied_db_app\appDBproj_MySql.sql") as db1
 import ("C:\Users\appDB\Documents\applied_db_app\appDBCity_Neo4j.sql") as db2
+
+def main():
+    name = input("Enter subject: ")
+    teacher = input("Enter teacher: ")
+    lc = input("Enter on Leaving Cert 1/0: ")
+
+    try:
+        num2DB.add_subject(name, teacher, lc)
+    except pymysql.err.ProgrammingError as e:
+        print("Programming Error:", e)
+    except pymysql.err.IntegrityError as e:
+        print("Subject", name, "already exists")
+    except Exception as e:
+        print("ERROR", e)
+
+
+if __name__ == "__main__":
+    main()
+
+
