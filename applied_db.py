a05d193bc004ef04a93313418002e09fbd3e88dc
Joanne <G00283773@atu.ie>
2024-04-30 21:13:42 +0100

    Adding menu selections and starting functions

--- applied_db.py
+++ applied_db.py
@@ -9,41 +9,68 @@ import pymysql.cursors
 import appDBproj_MySql
 import appDBCity_Neo4j
 
-# def add_person(person):
+# 3 Add person function
+def add_person(personID, personname, age, salary, city):
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
+
+    sql = "INSERT INTO person_table VALUES (%s, %s, %s, %s, %s)"
     
-
-def add_city(country, population, twinned_city):
-    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBCity_Neo4j", cursorclass=pymysql.cursors.DictCursor)
-
-    sql = "INSERT INTO person_table VALUES (%s, %s, %s)"
-
     with db:
         try:
             cursor = db.cursor()
-            cursor.execute(sql, (country, population, twinned_city))
+            cursor.execute(sql, (personID, personname, age, salary, city))
             db.commit()
-        except pymysql.err.IntegrityError as e:
-            print(e)
-        except pymysql.err.InternalError as e:
-            print (e)
+        except pymysql.err.IntegrityError as a:
+            print(a)
+        except pymysql.err.InternalError as a:
+            print (a)
         except Exception as e:
-            print(e)
+            print(a)
 
-def find_city(country):
-    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBCity_Neo4j", cursorclass=pymysql.cursors.DictCursor)
+# 1 View cities by country function
+def view_city_by_country(ID, Name, CountryCode, District, Population, latitude, longitude):
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
 
     sql = '''
             select ct.country, ct.population, ct.twinned_city
             from city_table ct
             inner join country_table cot
                 on ct.country = cot.country
-            where surname like %s
+            where name like %s
           '''
     with db:
         cursor = db.cursor()
         cursor.execute(sql, ("%"+country+"%"))
         return cursor.fetchall()
 
+# 2 Update city population function
+def update_city_pop(population):
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
 
+    sql = ...
 
+# 4 Delete person function
+def delete_person(personID, personname, age, salary, city):
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
 
+    sql = "DELETE person from person_table VALUES (%s, %s, %s, %s, %s)"
+
+    ...
+
+# 5 View countries by population function
+def view_country_by_pop(ID, Name, CountryCode, District, Population, latitude, longitude):
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
+
+    sql = ...
+
+# 6 Show twinned cities function
+def show_twin_cities(ID, Name, CountryCode, District, Population, latitude, longitude):
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
+
+    sql = 
+
+# 7 Twin with Dublin function
+def twin_with_dub(ID, Name, CountryCode, District, Population, latitude, longitude):
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
+
+    sql = 
--- display.py
+++ display.py
@@ -1,30 +1,57 @@
 # Python app for Applied Databases module - ATU Galway
 # By Joanne Feeney
 
+# Imports
 import applied_db
 
+# Main function
 def main():
     display_menu()
 
     while True:
         choice = input("Enter choice: ")
         if (choice == "1"):
-            country = input("Country: ")
+            ### View Cities by country
+            '''country = input("Country: ")
             population = input("Population: ")
             twinned_city = input("Twinned_city: ")
             applied_db.add_city(country, population, twinned_city)
-            display_menu()
+            display_menu()'''
+        elif (choice == "2"):
+            ### Update city population 
+        elif (choice == "3"):
+            ### Add new person
+        elif (choice == "4"):
+            ### Delete person
+        elif (choice == "5"):
+            ### view countries by population
+        elif (choice == "6"):
+            ### Show twinned cities
+        elif (choice == "7"):
+            ### Twin with Dublin
+        elif (choice == "x"):
+            print("Exiting application")
+            return True
         else:
-            break;
+            print("Invalid choice. Please try again.")
+            break
 
 
+# Display menu
 def display_menu():
     print("")
-    print("MENU")
-    print("====")
-    print("1 - Add country")
-    print("2 - Find country")
-    print("x- Exit")
+    print("==================================")
+    print("                MENU              ")
+    print("==================================")
+    print("")
+    print("1 - View Cities by Country")
+    print("2 - Update City Population")
+    print("3 - Add New Person")
+    print("4 - Delete Person")
+    print("5 - View Countries by Population")
+    print("6 - Show twinned cities")
+    print("7 - Twin with Dublin")
+    print("x - Exit application")
 
 if __name__ == "__main__":
     main()
