4e665e7dd55e89fdc9ad9791016a88f1e6530a23
Joanne <G00283773@atu.ie>
2024-05-12 20:32:43 +0100

    Functions and display code written

--- applied_db.py
+++ applied_db.py
@@ -6,83 +6,247 @@
 import pymysql
 import pymysql.cursors
 
-# 3 Add person function
-def add_person(personID, personname, age, salary, city):
-    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj", cursorclass=pymysql.cursors.DictCursor)
-
-    sql = "INSERT INTO person_table VALUES (%s, %s, %s, %s, %s)"
-    
-    with db:
-        try:
-            cursor = db.cursor()
-            cursor.execute(sql, (personID, personname, age, salary, city))
-            db.commit()
-        except pymysql.err.IntegrityError as a:
-            print(a)
-        except pymysql.err.InternalError as a:
-            print (a)
-        except Exception as a:
-            print(a)
 
 # 1 View cities by country function
 def view_city_by_country(id, Name, CountryCode, District, Population, latitude, longitude):
-    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj", cursorclass=pymysql.cursors.DictCursor)
 
-    sql = """
-    SELECT ID, Name, CountryCode, District, Population, latitude, longitude
-    FROM city_table
-    WHERE CountryCode = %s
-    """
+    sql_query = "SELECT * FROM {country_table} WHERE Name LIKE '%{name}%'"
 
     with db:
         cursor = db.cursor()
-        cursor.execute(sql, (id, Name, CountryCode, District, Population, latitude, longitude))
-        return cursor.fetchall()
-        cursor.close()
-        return results
+        cursor.execute(sql_query)
+        results = cursor.fetchall()
+        if len(results) == 0:
+                return None
 
+    # Using the country code to get the cities data from the city table
+    sql_query = "SELECT * FROM {city_table} WHERE CountryCode LIKE '{countrycode}'"
+    
+    with db:
+            cursor.execute(sql_query)
+            results = cursor.fetchall()
+    
+    except pymysql.err.InterfaceError as e:
+        print("Error: {e}")
 
-'''sql =
-            select ct.country, ct.population, ct.twinned_city
-            from city_table ct
-            inner join country_table cot
-                on ct.country = cot.country
-            where name like %s
+def get_city(city_id:str)-> list:
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj", cursorclass=pymysql.cursors.DictCursor)
+
+    # SQL query 
+    sql_query = "SELECT * FROM {city_table} WHERE ID = {city_id}"
 
     with db:
         cursor = db.cursor()
-        cursor.execute(sql, ("%"+country+"%"))
-        return cursor.fetchall()'''
+        cursor.execute(sql_query)
+        # Get the city details
+        city_details = cursor.fetchone()
+        # Print(city_details['Name'])
+        if city_details is None:
+            return ['','','']
+        else:
+            old_population = city_details["Population"]
+            lines = ['','']
+            for key in city_details.keys():
+                return  ["DISPLAY TABLE TO BE CREATED JF"]
+            
+    except pymysql.err.OperationalError as Er:
+        print('Error: {Er}')
+        
+    except Exception as Er:
+        print("Error in get_city : {Er} ")
 
+def get_city_id(city_name:str)-> str:
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj", cursorclass=pymysql.cursors.DictCursor)
 
-'''# 2 Update city population function
-def update_city_pop(population):
-    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
+    # SQL query to retrieve city details
+    sql_query = "SELECT * FROM {city_table} WHERE Name = '{city_name}'"
 
-    sql = ...
+    with db:
+        cursor = db.cursor()
+        cursor.execute(sql_query)
+        # Get the city details
+        city_details = cursor.fetchone()
+        if city_details is None:
+            return ''
+        else:
+        # Return the city id
+            return city_details['ID']
+        
+    except Exception as Er:
+        print("Error in get_city id : {Er} ")
+
+# 2 Update city population function
+def update_city_pop(city_id:str,new_population:int)->None:
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj", cursorclass=pymysql.cursors.DictCursor)
+
+    sql_query = "SELECT * FROM {city_table} WHERE ID = {city_id}"
+
+    with db:
+        cursor = db.cursor()
+        cursor.execute(sql_query)
+
+        try:
+            # Update city population
+            update_sql_query = "UPDATE {city_table} SET Population = {new_population} WHERE ID = {city_id}"
+            cursor.execute(update_sql_query)
+            # Retrieve city details after update
+            cursor.execute(sql_query)
+            city_details = cursor.fetchone()
+
+            print(create_display_table(lines=lines))
+            pymysql.commit()
+
+        except Exception as Er:
+            print("Error into get_city : {Er} ")
+
+# 3 Add person function
+def add_person(personID:str, personname:str, age:str, salary:str, city:str) -> None:
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj", cursorclass=pymysql.cursors.DictCursor)
+
+    '''sql_query = "INSERT INTO person_table VALUES (%s, %s, %s, %s, %s)"'''
+    
+    with db:
+        cursor = db.cursor()
+        cursor.execute(sql_query)
+        try:
+            ready_to_insert = True
+            # Check if the person exists
+            sql_query = "SELECT * FROM {person_table} WHERE personID LIKE '{personID}'"
+            cursor.execute(sql_query)
+            results = cursor.fetchall()
+            if len(results) != 0:
+                print("Error: Person ID {personID} already exists")
+                ready_to_insert = False
+            # Check if the city exists
+            sql_query = "SELECT * FROM {self.table_city} WHERE ID LIKE '{city}'"
+            cursor.execute(sql_query)
+            results = cursor.fetchall()
+            if len(results) == 0:
+                print("Error: City ID {city} does not exist")
+                ready_to_insert = False
+
+            # SQL query to insert new person
+            if ready_to_insert:
+                sql_query = "ALTER TABLE {person_table} DISABLE KEYS"
+                # Execute(sql_query)
+                sql_query = '''
+                    INSERT INTO {person_table} (personID,personname,age,salary,city) VALUES ({personID}, 
+                '{personname}', {age}, {salary}, {city})'''
+                cursor.execute(sql_query)
+                sql_query = "ALTER TABLE {person_table} ENABLE KEYS"
+                # Execute(sql_query)
+                pymysql.commit()
+        except Exception as Er:
+            print(Er)
+
+    def show_person(person) -> str:
+        try:
+            sql_query = "SELECT * FROM {person_table}"# WHERE personID = {personID}"
+            cursor = self.connection.cursor()
+            cursor.execute(sql_query)
+            results = cursor.fetchall()
+            lines = []
+            if len(results) == 0:
+                return ''
+        except Exception as Er:
+            print("Error: {Er}")
+            return ''
 
 # 4 Delete person function
-def delete_person(personID, personname, age, salary, city):
-    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
+def delete_person(personID:str):
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj", cursorclass=pymysql.cursors.DictCursor)
 
-    sql = "DELETE person from person_table VALUES (%s, %s, %s, %s, %s)"
+    with db:
+        cursor = db.cursor()
+        # SQL query to delete person
+    try:
+        sql_query = "SELECT * FROM {person_table} WHERE personID = '{personID}'"
+        cursor.execute(sql_query)
+            # Get the city details
+        person_details = cursor.fetchone()
+        if person_details is None:
+            print("Person ID not found")
+        else:
+            sql_query = "DELETE FROM {person_table} WHERE personID = {personID}"
+            cursor.execute(sql_query)
+            pymysql.commit()
+            print("Person ID: {personID} deleted")
 
-    ...
+    except pymysql.err.ProgrammingError as Er:
+        print("Error: {Er}")
+    except pymysql.err.OperationalError as Er:
+        print("Error: {Er}")
+    except pymysql.err.IntegrityError as Er:
+        print("Error: Can't delete Person ID: {personID}")
+    except Exception as Er:
+        print(Er)
 
 # 5 View countries by population function
-def view_country_by_pop(ID, Name, CountryCode, District, Population, latitude, longitude):
-    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
+def view_country_by_pop(population:str):
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj", cursorclass=pymysql.cursors.DictCursor)
 
-    sql = ...
+    with db:
+        cursor = db.cursor()
+
+    # Get country code by country name from the table
+    try:
+        population = int(population)
+        sql_query = "SELECT * FROM {table_country} WHERE Population {population}"
+        cursor.execute(sql_query)
+        results = cursor.fetchall()
+        if len(results) == 0:
+            return ''
+        else:
+            return create_display_table(lines=lines)
+    except Exception as Er:
+        print("Error in (view_country_by_pop func): {Er}")
+        return ''
 
 # 6 Show twinned cities function
-def show_twin_cities(ID, Name, CountryCode, District, Population, latitude, longitude):
-    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
+def show_twin_cities(Name) -> list:
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj", cursorclass=pymysql.cursors.DictCursor)
 
-    sql = 
+    with db:
+        cursor = db.cursor()
+
+    try:
+            if twinned_cities = []
+                    result = ("MATCH (a)-[r:TWINNED_WITH]->(b) RETURN a, r, b")
+
+                    # Print the results
+                    for record in result:
+                return twinned_cities
+        except Exception as Er:
+            print("Error in (show_twin_cities): {Er}")
 
 # 7 Twin with Dublin function
-def twin_with_dub(ID, Name, CountryCode, District, Population, latitude, longitude):
-    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj_MySql", cursorclass=pymysql.cursors.DictCursor)
+def twin_with_dub(city_name:str="Dublin") -> list:
+    db = pymysql.connect(host="localhost", user="root", password="root", db="appDBproj", cursorclass=pymysql.cursors.DictCursor)
 
-    sql = '''
+    with db:
+        cursor = db.cursor()
+        try:
+            twinned_with_city = []
+            with ("MATCH (d:City), (c:City) WHERE d.name = $name AND EXISTS((d)-[:TWINNED_WITH]->(c)) RETURN c.name AS name, c.cid AS id", 
+                  name=city_name
+            )
+                # Print the result
+                for record:
+                twinned_with_city.append(record['id'])
+            
+            return twinned_with_city
+        except Exception as Er:
+            print("Error in (neo4j_twinned_with func): {Er}")
+
+def twin_with_dub(city_id:str) -> bool:
+        try:
+            dublin_id = get_city_id(city_name='Dublin')
+            # Create a session
+            with self.neo4j_connection.session() as session:
+                run("MATCH (c1:City {cid: $cid1}), (c2:City {cid: $cid2}) CREATE (c1)-[:TWINNED_WITH]->(c2)", cid1=cid1, cid2=cid2)
+                
+                return True
+        except Exception as Er:
+            print("Error in (twin_with_dub func): {Er}")
+            return False
--- display.py
+++ display.py
@@ -2,56 +2,18 @@
 # By Joanne Feeney
 
 # Imports
-import applied_db
+import applied_db as applied_db
 
-# Main function
-def main():
-    display_menu()
-
-    while True:
-        choice = input("Enter choice: ")
-        if (choice == "1"):
-            ### View Cities by country
-            countrycode = input("Country Code: ")
-            applied_db.view_city_by_country(id, Name, CountryCode, District, Population, latitude, longitude)
-            print("Cities by Country: ")
-            display_menu()
-        if (choice == "2"):
-            ### Update city population 
-            print("TBD: ")
-        if (choice == "3"):
-            ### Add new person
-            personID = input("ID: ")
-            personname = input("Name: ")
-            age = input("Age: ")
-            salary = input("Salary: ")
-            city = input("City: ")
-            applied_db.add_person(personID, personname, age, salary, city)
-            print("Person added")
-            display_menu()
-        if (choice == "4"):
-            ### Delete person
-            print("TBD: ")
-        if (choice == "5"):
-            ### view countries by population
-            print("TBD: ")
-        if (choice == "6"):
-            ### Show twinned cities
-            print("TBD: ")
-        if (choice == "7"):
-            ### Twin with Dublin
-            print("TBD: ")
-        if (choice == "x"):
-            print("Exiting application")
-            return True
-        else:
-            print("Invalid choice. Please try again")
+# Defining integer conditions
+def get_numerical(message:str,error:str="Invalid input. Please enter a number")->int:
+    while True: # Loop until numerical input entered
+        user_input = input(message)
+        if user_input.isdigit():
+            user_input = int(user_input)
             break
-            '''country = input("Country: ")
-            population = input("Population: ")
-            twinned_city = input("Twinned_city: ")
-            applied_db.add_city(country, population, twinned_city)
-            display_menu()'''
+        else:
+            print(error)
+    return user_input
 
 # Display menu
 def display_menu():
@@ -68,6 +30,147 @@ def display_menu():
     print("6 - Show twinned cities")
     print("7 - Twin with Dublin")
     print("x - Exit application")
+
+# Main function
+def main():
+    display_menu()
+
+    while True:
+        choice = input("Enter choice: ")
+        if (choice == "1"):
+            ### View cities by country
+            country_code = input("Country Code: ")
+            applied_db.view_city_by_country(country_code=country_code)
+            if len(applied_db.result_city_list) == 0:
+                if len(applied_db.result_country_list[0]) == 0:
+                    print("No countries found for: " + country_code)
+                else:
+                    print("No cities found for country: " + country_code)
+            else:
+                print(applied_db.result_city_list[0][2])
+                display_menu()
+
+        elif (choice == "2"):
+            ### Update city population 
+            city_id = (message="Enter a city ID: ",numerical = True)
+            applied_db.get_city(city_id=city_id)
+
+            feedback,old_population,_ = applied_db.get_city(city_id=city_id)
+            if feedback == '':
+                print("No city found with ID = {city_id}")
+            else:
+                print(feedback)
+                old_population = int(old_population)
+
+                user_input = (message="[I]ncrease/[D]ecrease Population: ",numerical = False,
+                    select_from = ['i','d'],error = "Invalid input. Please enter I or D"
+                )
+                user_input = user_input.upper()
+
+                amount = (message="Enter Population {['Increase','Decrease'][['I','D'].index(user_input)]}: ",
+                    numerical = True,error="Invalid input. Please enter the amount of {['Increase',
+                        'Decrease'][['I','D'].index(user_input)]} (must be a number)"
+                )
+                amount = int(amount)
+                if user_input == 'I':
+                    new_population = old_population + amount
+                else:
+                    new_population = old_population - amount
+
+                print("Old Population: {old_population} \n New Population: {new_population}")
+                applied_db.update_city_pop(city_id=city_id,new_population=new_population)
+                display_menu()
+
+        elif (choice == "3"):
+            ### Add new person
+            print("Enter person details: ")
+            personID = (message='ID: ',numerical = True,error = "Invalid input. Please enter a number")
+            personname = (message='Name: ',numerical = False,error = "Invalid input")
+            age = (message='Age: ',numerical = True,error = "Invalid input. Please enter a number")
+            salary = (message='Salary: ',numerical = True,error = "Invalid input. Please enter a number")
+            city = (message='City: ',numerical = True,error = "Invalid input. Please enter a number")
+
+            # person_details = input("Enter person details (ID, Name, Age, Salary, City): ")
+            applied_db.add_person(personID=personID,
+                                   personname=personname,
+                                   age=age,
+                                   salary=salary,
+                                   city=city)
+            display_menu()
+
+        elif (choice == "4"):
+            ### Delete person
+            person_id = (message="Enter person ID to be deleted: ",
+                numerical = True,error = "Invalid input. Please enter person ID"
+            )
+            applied_db.delete_person(personID=person_id)
+
+            print(applied_db.show_person())
+            display_menu()
+
+        elif (choice == "5"):
+            ### View countries by population
+            operation = (message="Enter either < or > or =: ",numerical = False,
+                select_from = ['>','<','='],error = "Invalid input, please choose one operation < or > or ="
+            )
+            population = (message="Enter Population: ",numerical = True,
+                error = "Invalid input, please enter a number"
+            )
+
+            print(applied_db.view_country_by_pop(operation=operation,population=population))
+            display_menu()
+
+        elif (choice == "6"):
+            ### Show twinned cities
+            twinned_cities = applied_db.show_twin_cities()
+            if twinned_cities == []:
+                print("Invalid Neo4J connection")
+                else:
+                print("No twinned cities found")
+            else:
+                sorted_list = sorted(twinned_cities)
+                print("Twinned Cities")
+                print("--------------")
+                for city in sorted_list:
+                    print("{city[0]} <-> {city[1]}")
+                    display_menu()
+        elif (choice == "7"):
+            ### Twin with Dublin
+            city_id = (message="Enter ID of City to twin with Dublin: " ,numerical = True)
+
+            feedback,_,city_details = applied_db.get_city(city_id=city_id)
+            
+            # Check if city exists in MySQL
+            if feedback == '':
+                print("Error. City ID does not exist in MySQL database")
+            elif city_details["Name"] == "Dublin":
+                print("Dublin can not be twinned with itself")
+            else:
+                neo4j_cities= applied_db.get_neo4j_cities()
+                # Check if dublin exists in Neo4j
+                dublin_id = applied_db.get_city_id(city_name="Dublin")
+                if dublin_id not in neo4j_cities:
+                    print("Error. Dublin does not exist in Neo4J database")
+                else:
+                    # Check if city is in Neo4j
+                    if str(city_id) in neo4j_cities or int(city_id) in neo4j_cities:
+                        # Check if city is already twinned with Dublin
+                        already_twinned = applied_db.neo4j_twinned_with(city_name="Dublin")
+                        if city_id in already_twinned:
+                            print("City ID is already twinned with Dublin")
+                        else:
+                            # Twin the city
+                            if applied_db.twin_with_dub(city_id=city_id):
+                                print("Dublin is now twinned with" {city_details['Name']})
+                                display_menu()
+
+        elif (choice == "x"):
+            print("Exiting application")
+            return True
+        
+        else:
+            print("Invalid choice. Please try again")
+            break
 
 if __name__ == "__main__":
     main()
